9ff7c3702a10dfac55f7f9af4408b0219b44f337
Chiu Wah Li (iMac) <chiuwah@gmail.com>
2020-06-14 11:40:18 +0200

    added ui

--- pages/admin/new-post/index.vue
+++ pages/admin/new-post/index.vue
@@ -1,7 +1,62 @@
 <template>
   <div class="admin-new-post-page">
     <section class="new-post-form">
-      <form></form>
+      <form @submit.prevent="onSave">
+        <AppControlInput v-model="editedPost.author">Author Name</AppControlInput>
+
+        <AppControlInput v-model="editedPost.title">Title</AppControlInput>
+
+        <AppControlInput v-model="editedPost.thumbnailLink">Thumbnail Link</AppControlInput>
+
+        <AppControlInput control-type="textarea" v-model="editedPost.content">Content</AppControlInput>
+
+        <AppButton type="submit">Save</AppButton>
+
+        <AppButton
+          type="button"
+          style="margin-left: 10px"
+          btn-style="cancel"
+          @click="onCancel"
+        >Cancel</AppButton>
+      </form>
     </section>
   </div>
 </template>
+
+<script>
+import AppButton from "@/components/UI/AppButton";
+import AppControlInput from "@/components/UI/AppControlInput";
+
+export default {
+  components: {
+    AppButton,
+    AppControlInput
+  },
+  data() {
+    return {
+      editedPost: {
+        author: "",
+        title: "",
+        thumbnailLink: "",
+        content: ""
+      }
+    };
+  },
+  methods: {
+    onSave() {
+      // Save the post
+      console.log(this.editedPost);
+    },
+    onCancel() {
+      this.$router.push("/admin");
+    }
+  }
+};
+</script>
+
+<style scoped>
+.admin-new-post-page {
+  width: 80%;
+  margin: auto;
+}
+</style>
